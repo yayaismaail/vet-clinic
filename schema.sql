6a473551fbf0010662e0d12a436767e90ff877ff
unknown <ismail halliru muhammad>
2023-11-07 10:22:33 +0100

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
+-- CREATE TABLE animals (
+--     name varchar(100)
+-- );
 CREATE TABLE animals (
-    name varchar(100)
+   id serial PRIMARY KEY,
+   name varchar(200),
+   date_of_birth date,
+   escape_attempts integer,
+   neutered boolean,
+   weight_kg decimal
 );
